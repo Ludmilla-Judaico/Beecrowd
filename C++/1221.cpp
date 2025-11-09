e18773754b3ec4868fcb9ec2d664dbfca54b31a9
Ludmilla Judaico <ludmillajudaico@gmail.com>
2025-11-09 13:17:24 -0300

    1221.cpp

---
+++ C++/1221.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool primo(long long num){
+  if(num == 1) return false;
+  if(num == 2) return true;
+  if(num % 2 == 0) return false;
+  for(long long i=3 ; i*i<=num ; i+=2){
+    if(num % i == 0) return false;
+  }
+  return true;
+}
+
+int main() 
+{
+  long long t; cin>>t;
+  while(t--){
+    long long n; cin>>n;
+    if(primo(n)) cout << "Prime\n";
+    else cout << "Not Prime\n";
+  }
+  
+  return 0;
+}
